6a9d9d26abe708cac38513d525b2ca61ea88e1fb
Monte <monte6198@gmail.com>
2022-10-06 16:15:51 +0900

    MODIFY add page

--- src/routes/Add.js
+++ src/routes/Add.js
@@ -1,7 +1,48 @@
+import "../css/Add.css";
+import Track from "../components/Track";
+
 function Add() {
   return (
-    <div>
-      <h1>this is Add</h1>
+    <div className="add">
+      <div className="row">
+        <div className="col-6 show">
+          <Track></Track>
+        </div>
+        <div className="col-6 add-info">
+          <h2 style={{ "margin-bottom": "40px" }}>Add your music</h2>
+          <div className="row">
+            <div className="col-4">
+              <h5>Title</h5>
+            </div>
+            <div className="col-8">
+              <input placeholder="Title" className="info-pad"></input>
+            </div>
+            <br />
+            <div className="col-4">
+              <h5>Artist Name</h5>
+            </div>
+            <div className="col-8">
+              <input placeholder="Artist Name" className="info-pad"></input>
+            </div>
+            <br />
+            <div className="col-4">
+              <h5>Music File</h5>
+            </div>
+            <div className="col-8">
+              <input type={"file"} className="info-pad"></input>
+            </div>
+            <br />
+            <div className="col-4">
+              <h5>Cover Image</h5>
+            </div>
+            <div className="col-8">
+              <input type={"file"} className="info-pad"></input>
+            </div>
+            <br />
+          </div>
+          <button style={{ "margin-top": "10px" }}>Add</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/store/trackSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let track = createSlice({
+  name: "Tracks",
+  initialState: [],
+  reducers: {},
+});
+
+// export let { addCart, addCount } = items.actions;
+export default Tracks;
