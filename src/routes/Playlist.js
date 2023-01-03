0cba49e1e3387b2c8b5a8d2d1e6e86bbb0fa8ca1
Monte <monte6198@gmail.com>
2023-01-03 23:05:17 +0900

    feat: 재생목록 추가

---
+++ src/routes/Playlist.js
@@ -0,0 +1,33 @@
+function Playlist() {
+  return (
+    <div className='player'>
+      <div style={{ marginBottom: '35px' }}>
+        <h2 style={{ textShadow: '0 0 3px white' }}>Playlists</h2>
+      </div>
+      <hr />
+      <div className='listOne'>
+        <img src='' alt='' />
+        <h5>Playlist Title</h5>
+        <h6>Playlist Generation Date</h6>
+        <br />
+      </div>
+      <hr />
+      <div className='listOne'>
+        <img src='' alt='' />
+        <h5>Playlist Title</h5>
+        <h6>Playlist Generation Date</h6>
+        <br />
+      </div>
+      <hr />
+      <div className='listOne'>
+        <img src='' alt='' />
+        <h5>Playlist Title</h5>
+        <h6>Playlist Generation Date</h6>
+        <br />
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default Playlist;
